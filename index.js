173f63b31cd4855fb28517ead716b4332262ae8a
kcritesh <kcritesh2059@gmail.com>
2023-03-13 12:50:23 +0545

    Heroku: Port Set for Deployment

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/", async (req, res) => {
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => {
+    app.listen(process.env.PORT || 8080, () => {
       console.log("Server has started on http://localhost:8080");
     });
   } catch (error) {
